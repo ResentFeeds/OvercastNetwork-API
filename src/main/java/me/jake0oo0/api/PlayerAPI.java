808eb4d1464ca08fa5f8d09b660f09377e379f71
Jake <jake0oo0andminecraft@gmail.com>
2013-12-05 17:57:25 -0600

    Refactor API classes

--- src/main/java/me/jake0oo0/api/PlayerAPI.java
+++ src/main/java/me/jake0oo0/api/PlayerAPI.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2013 Jake0oo0
+ * Copyright (c) <year> <copyright holders>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,21 +23,14 @@
  */
 package me.jake0oo0.api;
 
-import me.jake0oo0.parsers.StatParser;
-import me.jake0oo0.parsers.TeamParser;
-import me.jake0oo0.stats.TeamStat;
 import me.jake0oo0.types.OvercastPlayer;
-import me.jake0oo0.types.OvercastTeam;
-import me.jake0oo0.types.OvercastTournament;
-import me.jake0oo0.types.TournamentTeam;
 
 /**
- * API class for the Overcast API
+ * Class for managing the retrieval of player objects
  *
  * @author Jake0oo0
  */
-public class API {
-
+public class PlayerAPI {
 	/**
 	 * Get an instance of an OvercastNetwork player
 	 *
@@ -60,58 +53,5 @@ public class API {
 	public OvercastPlayer getPlayerByUrl(String url) {
 		String user = url.split("https://oc.tc/")[1];
 		return getPlayer(user);
-	}
-
-	/**
-	 * Get the stats of an Overcast team
-	 *
-	 * @param team input OvercastTeam
-	 * @return TeamStat's of input team
-	 */
-	public TeamStat getOvercastTeamStats(OvercastTeam team) {
-		return StatParser.parseOvercastTeam(team.getPlayers());
-	}
-
-	/**
-	 * Get the stats of a tournament team
-	 *
-	 * @param team input TournamentTeam
-	 * @return team's stats
-	 */
-	public TeamStat getTourneyTeamStats(TournamentTeam team) {
-		return StatParser.parseTournamentTeam(team.getPlayers());
-	}
-
-	/**
-	 * Get a tourney team from the tourney url and name
-	 *
-	 * @param url tourney url
-	 * @param name team name
-	 * @return TournamentTeam created from params
-	 */
-	public TournamentTeam getTourneyTeam(String url, String name) {
-		return TeamParser.parseTournamentTeam(url, name);
-	}
-
-	/**
-	 * Retrieve stats of an OvercastTeam
-	 *
-	 * @param url team url
-	 * @return ouput OvercastTeam
-	 */
-	public OvercastTeam getOvercastTeam(String url) {
-		//TODO
-		return null;
-	}
-
-	/**
-	 * Get an instance of an OvercastTournament
-	 *
-	 * @param url tourney url
-	 * @return an OvercastTournament from the url
-	 */
-	public OvercastTournament getTournament(String url) {
-		//TODO
-		return null;
 	}
 }
---
+++ src/main/java/me/jake0oo0/api/TeamAPI.java
@@ -0,0 +1,57 @@
+package me.jake0oo0.api;
+
+import me.jake0oo0.parsers.StatParser;
+import me.jake0oo0.parsers.TeamParser;
+import me.jake0oo0.stats.TeamStat;
+import me.jake0oo0.types.OvercastTeam;
+import me.jake0oo0.types.TournamentTeam;
+
+/**
+ * Class for managing the retrieval of team stats
+ *
+ * @author Jake0oo0
+ */
+public class TeamAPI {
+
+	/**
+	 * Get the stats of an Overcast team
+	 *
+	 * @param team input OvercastTeam
+	 * @return TeamStat's of input team
+	 */
+	public TeamStat getOvercastTeamStats(OvercastTeam team) {
+		return StatParser.parseOvercastTeam(team.getPlayers());
+	}
+
+	/**
+	 * Get the stats of a tournament team
+	 *
+	 * @param team input TournamentTeam
+	 * @return team's stats
+	 */
+	public TeamStat getTourneyTeamStats(TournamentTeam team) {
+		return StatParser.parseTournamentTeam(team.getPlayers());
+	}
+
+	/**
+	 * Get a tourney team from the tourney url and name
+	 *
+	 * @param url tourney url
+	 * @param name team name
+	 * @return TournamentTeam created from params
+	 */
+	public TournamentTeam getTourneyTeam(String url, String name) {
+		return TeamParser.parseTournamentTeam(url, name);
+	}
+
+	/**
+	 * Retrieve stats of an OvercastTeam
+	 *
+	 * @param url team url
+	 * @return ouput OvercastTeam
+	 */
+	public OvercastTeam getOvercastTeam(String url) {
+		//TODO
+		return null;
+	}
+}
